d0ffa877e829223ce039cab154f58500f16a5faa
BrianFaviansa <brianfpd31@gmail.com>
2024-07-21 19:09:40 +0700

    feat: change title and register page UI

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,13 +1,35 @@
 import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import PasswordInput from "../../components/Input/PasswordInput";
+import { Link } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const handleRegister = async (e) => {
     e.preventDefault();
+
+    if (!name) {
+      setError("Please enter your name.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password.");
+      return;
+    }
+    setError("");
+
+    // register API call
   };
 
   return (
@@ -20,11 +42,32 @@ const Register = () => {
             <h4 className="text-2xl mb-7">Register</h4>
             <input
               type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="input-box"
+            />
+            <input
+              type="text"
               placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="input-box"
             />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            <button type="submit" className="btn-primary">
+              Create Account
+            </button>
+            <p className="text-sm text-center mt-4">
+              Already have an account?{" "}
+              <Link to="/login" className="font-medium text-primary underline">
+                Sign in
+              </Link>
+            </p>
           </form>
         </div>
       </div>
