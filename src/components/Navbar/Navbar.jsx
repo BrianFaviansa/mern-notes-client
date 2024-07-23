c8af09c3c56cd28b13788a930f025d202b830afa
BrianFaviansa <brianfpd31@gmail.com>
2024-07-23 07:49:09 +0700

    feat: get user api integration

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,10 +3,11 @@ import ProfileInfo from "./../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
-const Navbar = () => {
+const Navbar = ({ userInfo }) => {
   const [searchQuery, setSearchQuery] = useState("");
-  const navigate = useNavigate;
+  const navigate = useNavigate();
   const onLogout = () => {
+    localStorage.clear();
     navigate("/login");
   };
   const handleSearch = () => {};
@@ -20,13 +21,13 @@ const Navbar = () => {
 
       <SearchBar
         value={searchQuery}
-        onChange={({target}) => {
+        onChange={({ target }) => {
           setSearchQuery(target.value);
         }}
         handleSearch={handleSearch}
         onClearSearch={onClearSearch}
       />
-      <ProfileInfo onLogout={onLogout} />
+      <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
     </div>
   );
 };
